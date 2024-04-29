35e659bd5c3e664575dde369a2b672023ae07c94
ppico20 <p.pico20@imperial.ac.uk>
2024-04-29 14:06:18 +0100

    Adjusted LSTM to predict first time of window

--- Reg_singleVar/ak0_tilde/lstm/HPT_Reg_lstm_ak0_tilde.py
+++ Reg_singleVar/ak0_tilde/lstm/HPT_Reg_lstm_ak0_tilde.py
@@ -41,59 +41,57 @@ tf.config.threading.set_intra_op_parallelism_threads(1)
 
 num_simulations = 152
 sequence_length = 101
-num_parameters = 6
+output_dim = 6
+input_dim = 1
 prediction_window = 20
 num_windows = sequence_length - prediction_window
 
 ################################## Reading csv ##################################
 
 dataset = pd.read_csv("../../../compiled_data/data_params_HST.csv", engine='python')
-parameters_train = dataset[['Time','epsilon','rho_r','mu_r','La_l','Bo_l']]
+parameters_train = dataset[['Time_tilde','epsilon','rho_r','mu_r','La_l','Bo_l']]
 
 scaler = preprocessing.MinMaxScaler()
 scaler.fit(parameters_train)
 
-#parameters_train_scaled = scaler.transform(parameters_train)
+parameters_train_scaled = scaler.transform(parameters_train)
 
-parameters_train_scaled = parameters_train.values
-
-#parameters_train_scaled = parameters_train_scaled.values
-train_parameter = parameters_train_scaled.reshape(num_simulations,sequence_length,num_parameters)
+#parameters_train_scaled = parameters_train.values
+train_parameter = parameters_train_scaled.reshape(num_simulations,sequence_length,output_dim)
 
 ak0_tilde_train = dataset[['ak0_tilde']]
 ak0_tilde_train = ak0_tilde_train.values
-ak0_in = ak0_tilde_train.reshape(num_simulations,sequence_length,1)
+ak0_in = ak0_tilde_train.reshape(num_simulations,sequence_length,input_dim)
 
 ############################################################
 
-input_seq = np.zeros((1,prediction_window,1))
+input_seq = np.zeros((1,prediction_window,input_dim))
 for i in range(num_simulations):
   current_traj = ak0_in[i,:,:]
 
   for start in range(num_windows):
-    input_seq = np.concatenate((input_seq,current_traj[start:(start+prediction_window),:].reshape(1,prediction_window,1)),axis=0)
+    input_seq = np.concatenate((input_seq,current_traj[start:(start+prediction_window),:].reshape(1,prediction_window,input_dim)),axis=0)
 
 input_seq = input_seq[1:,:,:]
 print(input_seq.shape)
 
 ############################################################
 
-train_parameter_shifted = np.zeros((num_simulations * (sequence_length - prediction_window), num_parameters))
+train_parameter_shifted = np.zeros((num_simulations * (sequence_length - prediction_window), output_dim))
 
 index = 0
 for i in range(num_simulations):
     for j in range(num_windows):
-        shifted_parameters = train_parameter[i, j + 1 : j + 1 + prediction_window, :]
-        train_parameter_shifted[index] = shifted_parameters[-1]
+        shifted_parameters = train_parameter[i, j : j + prediction_window, :]
+        train_parameter_shifted[index] = shifted_parameters[0]
         index += 1
 
 print(train_parameter_shifted.shape)
+
 ################################################################
 
-input_dim = 1
-hidden_size=100
-
-use_dropout=True
+hidden_size = 100
+use_dropout = True
 
 model = Sequential()
 
@@ -102,11 +100,11 @@ model.add(LSTM(hidden_size,input_shape=(prediction_window,input_dim),activation=
 model.add(Dense(64))
 #model.add(Dense(100))
 model.add(LeakyReLU(alpha=0.2))
-model.add(Dense(num_parameters))
+model.add(Dense(output_dim))
 #model.add(LeakyReLU(alpha=0.2))
 
 #optimizer = optimizers.Adam()
-optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+optimizer = keras.optimizers.Adam(learning_rate=0.001)
 model.summary()
 
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
